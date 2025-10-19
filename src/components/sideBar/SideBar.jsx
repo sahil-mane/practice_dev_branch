f9357b0a14fe49bcea2432e9dfd025ee3498c55c
sahil-mane <manesahil238@gmail.com>
2025-10-19 13:54:31 +0530

    sideBar design

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const SideBar = () => {
+
+  const sideBarData = [
+    {label:"Layout 1",link:"/"},
+    {label:"Layout 2",link:"/"},
+    {label:"Layout 3",link:"/"},
+  ]
+
+  const navigate = useNavigate();
+
   return (
-    <div style={{border:"1px solid red"}}>
-        <h1>LOGO</h1>
+    <div style={{border:"1px solid red",height:"100%"}}>
+        <h1 style={{height:"8%"}}>LOGO</h1>
+        <div style={{border:"1px solid blue",display:"flex",flexDirection:"column",gap:"5px"}}>
+          {sideBarData.map((side)=>{
+            return(
+              <p style={{cursor:"pointer"}} onClick={()=>navigate(side.link)}>{side.label}</p>
+            )
+          })}
+        </div>
     </div>
   )
 }
